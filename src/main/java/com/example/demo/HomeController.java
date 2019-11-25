416981fb006aaf804328fda99d5a384ff08581e9
fenglin098 <feng.lin098@gmail.com>
2019-11-25 10:55:29 -0500

    first commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,73 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+    @Autowired
+    JobRepository jobRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
+
+    @RequestMapping("/")
+    public String listJobs(Model model){
+        model.addAttribute("jobs", jobRepository.findAll());
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String JobForm(Model model){
+        model.addAttribute("job", new Job());
+        return "jobform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@ModelAttribute Job job, @RequestParam("file") MultipartFile file){
+        if (file.isEmpty()){
+            jobRepository.save(job);
+            return "redirect:/";
+        }
+        try {
+            Map uploadResult = cloudc.upload(file.getBytes(),
+                    ObjectUtils.asMap("resourcetype","auto"));
+            job.setPicture(uploadResult.get("url").toString());
+            jobRepository.save(job);
+        }catch (IOException e){
+            e.printStackTrace();
+            return "redirect:/add";
+        }return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showJob(@PathVariable("id") long id, Model model){
+        model.addAttribute("job", jobRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateJob(@PathVariable("id") long id, Model model){
+        model.addAttribute("job", jobRepository.findById(id).get());
+        return "jobform";
+    }
+
+    @RequestMapping("delete/{id}")
+    public String deleteJob(@PathVariable("id") long id, Model model){
+        jobRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @PostMapping("/result")
+    public String search(Model model, @RequestParam("search") String search){
+        model.addAttribute("jobs", jobRepository.findByTitleContainingIgnoreCase(search));
+        return "result";
+    }
+}
---
+++ src/main/java/com/example/demo/Job.java
@@ -0,0 +1,76 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title;
+    private String description;
+    private String postedDate;
+    private String author;
+    private String phone;
+    private String picture;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPostedDate() {
+        return postedDate;
+    }
+
+    public void setPostedDate(String postedDate) {
+        this.postedDate = postedDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
